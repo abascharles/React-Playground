84c553424cc6ec653607be4262ee8409a63c0ff6
abascharles <abascharlesbenard@gmail.com>
2024-03-27 08:23:48 +0300

    Rendering lists in react

--- src/List.jsx
+++ src/List.jsx
@@ -1,9 +1,25 @@
 function List(){
-    const fruits = ['Apples', 'Oranges','Banana', 'Coconut', 'Pineapple' ];
 
-    // Converting array of string into an array of list item elements
-    const listItems = fruits.map(fruit => <li>{fruit}</li>);
-    return(<ul>{listItems}</ul>);
+
+    // sort items in our lists
+    // fruits.sort((a, b) => a.name.localeCompare(b.name));  //Alphabetical
+    // fruits.sort((a, b) => a.name.localeCompare(b.name));  //reverse Alphabetical
+
+    // Sorting by calories numeric
+    // fruits.sort((a,b) => a.calories -b.calories);  //Numeric order/ascending
+    // fruits.sort((a,b) => b.calories -a.calories) //reverse Numeric order/descending
+
+    // using filter method to filter out list items
+
+    // const lowCalFruits = fruits.filter(fruit => fruit.calories<100);  //finding low calorie fruits
+
+    // Converting array of string into an array of list item elements using map method
+    const listItems = fruits.map(fruit => <li key = {fruit.id}>
+                                                                    {fruit.name}: &nbsp;
+                                                                 <b> {fruit.calories}</b>  </li>);
+    return(<ol>{listItems}</ol>);
+
+//  converting array os strings to array of abjects.
 }
 
 export default List;
